844ff0f8b26a57b5e99e45b23b5ce2d3460a110c
Vanille <beernaert.v@gmail.com>
2023-06-06 19:50:40 +0200

    Update button UX and click effect on the button

--- share-button.js
+++ share-button.js
@@ -1,36 +1,53 @@
-function ShareButtonEventDefi() {
-    const ShareButtonEventListener = document.getElementById('share-button');
-    let ShareSectionParent = document.querySelector('.share-social-network-button');
-  
-    const handleClick = () => {
-      if (window.screen.width <= 768) {
-        console.log("Screen < 768px");
-        if (navigator.share) {
-          navigator.share({
-            title: 'Title',
-            text: 'Text',
-            url: 'https://google.com',
-          })
-          .then(() => console.log('Check'))
-          .catch((error) => console.log('Erreur ', error));
-        }
-      } else {
-        console.log("Screen width > 769px");
-        console.log('click: Screen width > 769px');
-        console.log('Link: https://google.com copied');
-        navigator.clipboard.writeText('https://google.com');
-        
-        ShareButtonEventListener.textContent = 'Link copied to clipboard';
-  
-        // Restaure le texte initial après 2 secondes
-        setTimeout(() => {
-          ShareButtonEventListener.textContent = 'Share';
-        }, 2000);
+function ShareButtonAction() {
+  const shareButtonEventListener = document.getElementById('share-button');
+  let shareSectionParent = document.querySelector('.share-social-network-button');
+
+  const handleClick = () => {
+    if (window.screen.width <= 768) {
+      console.log("Screen < 768px");
+      if (navigator.share) {
+        navigator.share({
+          title: 'Title',
+          text: 'Text',
+          url: 'https://google.com',
+        })
+        .then(() => console.log('Check'))
+        .catch((error) => console.log('Erreur ', error));
       }
-    };
-  
-    ShareButtonEventListener.addEventListener('click', handleClick);
-  }
-  
-  ShareButtonEventDefi();
-  
+    } else {
+      console.log("Screen width > 769px");
+      // console.log('click: Screen width > 769px');
+      console.log('Link: https://google.com copied');
+      navigator.clipboard.writeText('https://google.com');
+      
+      shareSectionParent.classList.add('clicked');
+      let divTextCopyLink = document.createElement('div');
+      divTextCopyLink.className = 'link-class';
+
+      setTimeout(() => {
+        divTextCopyLink.className = 'link-class fade-in';
+      }, 100);
+
+      divTextCopyLink.textContent = 'Link copied !';
+
+      shareSectionParent.insertAdjacentElement('afterend', divTextCopyLink);
+
+      setTimeout(() => {
+        shareButtonEventListener.textContent = 'Share this link';
+
+        setTimeout(() => {
+          shareSectionParent.classList.remove('clicked');
+          divTextCopyLink.classList.add('fade-out');
+
+          setTimeout(() => {
+            divTextCopyLink.remove();
+          }, 300);
+        }, 200);
+      }, 1500);
+    }
+  };
+
+  shareButtonEventListener.addEventListener('click', handleClick);
+}
+
+ShareButtonAction();
